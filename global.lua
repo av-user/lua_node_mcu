5761404db31ebb85b06d6d89d29afb83e0845cba
av-user <anatolij_vasilcenko@gmx.net>
2017-12-22 14:51:15 +0100

    Create global.lua
    the file with all user-specific data

---
+++ global.lua
@@ -0,0 +1,10 @@
+WIFI_CONN_TRYCOUNT=10
+SSID="<your access point ssid>"
+PWD_WIFI="<your access point password>"
+
+MY_EMAIL = "<your email>@gmail.com"
+MY_CREDENTIALS = "<your gmail credentials>"
+SMTP_SERVERNAME = "smtp.gmail.com"
+SMTP_PORT = "465"
+MAIL_TO = "<your recipient's email>"
+SUBJECT = "<your email subject>"
